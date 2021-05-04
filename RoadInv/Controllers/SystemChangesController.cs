dfe17d1170a845291c88818e34acf1b21647437f
Michael White <Michael.White@ardot.gov>
2021-05-04 16:25:17 -0500

    code cleanup

--- RoadInv/Controllers/SystemChangesController.cs
+++ RoadInv/Controllers/SystemChangesController.cs
@@ -24,12 +24,12 @@ namespace RoadInv.Controllers
         // GET: 
         [Route("system_changes/nhs")]
         [Route("system_changes/nhs.html")]
-        public async Task<IActionResult> system_changes_nhs(string sortOrder, string district, string county, string route, string section, decimal? logmile, decimal? blm, decimal? elm, string direction, string nhs, int? page, string dissolve)
-        {
+        public async Task<IActionResult> system_changes_nhs(SystemChangesPageModel pageModel, int? page, string dissolve)
+        { //TODO: This function needs to be split up into smaller functions. It's taking way too many arguments. Could probably just pass an object as the parameter
             int pageSize = 50;
             int pageNumber = (page ?? 1); //TODO: separate paging for excludeNHS table
 
-            var roads = from r in _context.RoadInvs
+            var roads = from r in _context.RoadInvs //select * from RoadInv where Nhs !=0
                         where r.Nhs != "0"
                         select r;
 
@@ -39,123 +39,211 @@ namespace RoadInv.Controllers
             var diss = from r in _context.DissolveNhsViews
                        select r;
 
-            var mymodel = new SystemChangesPageModel();
+
             var validationModel = new ValidationModel(_context);
-            
-            mymodel.Districts = validationModel.AH_District.ConvertAll(a => { return new SelectListItem { Text = a.DistrictNumber, Value = a.DistrictNumber, Selected = false }; }) ;
-            mymodel.Counties = validationModel.AH_County.ConvertAll(a => { return new SelectListItem { Text = a.CountyNumber + " - "+ a.County, Value = a.CountyNumber, Selected = false }; });
-            mymodel.Directions = validationModel.LOG_DIRECT.ConvertAll(a => { return new SelectListItem { Text = a.Domainvalue, Value = a.Domainvalue, Selected = false }; });
-            mymodel.NHS_vals = validationModel.NHS.ConvertAll(a => { return new SelectListItem { Text = a.Domainvalue, Value = a.Domainvalue, Selected = false }; });
-            mymodel.Dissolve = dissolve;
-          
+
+            pageModel.Districts = validationModel.AH_District.ConvertAll(a => { return new SelectListItem { Text = a.DistrictNumber, Value = a.DistrictNumber, Selected = false }; });
+            pageModel.Counties = validationModel.AH_County.ConvertAll(a => { return new SelectListItem { Text = a.CountyNumber + " - " + a.County, Value = a.CountyNumber, Selected = false }; });
+            pageModel.Directions = validationModel.LOG_DIRECT.ConvertAll(a => { return new SelectListItem { Text = a.Domainvalue, Value = a.Domainvalue, Selected = false }; });
+            pageModel.NHS_vals = validationModel.NHS.ConvertAll(a => { return new SelectListItem { Text = a.Domainvalue, Value = a.Domainvalue, Selected = false }; });
+            pageModel.Dissolve = dissolve;
 
 
-            ViewBag.CurrentSort = sortOrder;
 
-            roads = roads.OrderBy(r => r.Nhs); 
+            ViewBag.CurrentSort = pageModel.SortOrder;
 
-            if (district != null)
+            roads = roads.OrderBy(r => r.Nhs);
+
+
+
+            if (!String.IsNullOrEmpty(pageModel.District))
             {
-                page = 1;
-                mymodel.District = district;
+                roads = roads.Where(r => r.AhDistrict.Equals(pageModel.District));
             }
-            if (county != null)
+            if (!String.IsNullOrEmpty(pageModel.County))
             {
-                page = 1;
-                mymodel.County = county;
+                roads = roads.Where(r => r.AhCounty.Equals(pageModel.County));
             }
-            if (route != null)
+            if (!String.IsNullOrEmpty(pageModel.Route))
             {
-                page = 1;
-                mymodel.Route = route;
+                roads = roads.Where(r => r.AhRoute.Equals(pageModel.Route));
             }
-            if (section != null)
+            if (!String.IsNullOrEmpty(pageModel.Section))
             {
-                page = 1;
-                mymodel.Section = section;
+                roads = roads.Where(r => r.AhSection.Equals(pageModel.Section));
             }
-            if (logmile != null)
+            if (!pageModel.Logmile.Equals(null))
             {
-                page = 1;
-                mymodel.Logmile = logmile;
+                roads = roads.Where(r => r.AhBlm.Equals(pageModel.Logmile) || r.AhElm.Equals(pageModel.Logmile));
             }
-            if(blm != null)
+            if (!pageModel.BLM.Equals(null))
             {
-                page = 1;
-                mymodel.BLM = blm;
+                roads = roads.Where(r => r.AhBlm.Equals(pageModel.BLM));//redundant
             }
-            if (blm != null)
+            if (!pageModel.ELM.Equals(null))
             {
-                page = 1;
-                mymodel.ELM = elm;
+                roads = roads.Where(r => r.AhBlm.Equals(pageModel.ELM));
             }
-            if(direction != null)
+            if (!String.IsNullOrEmpty(pageModel.Direction))
             {
-                page = 1;
-                mymodel.Direction = direction;
+                roads = roads.Where(r => r.LogDirect.Equals(pageModel.Direction));
             }
-            if(nhs != null)
+            if (!String.IsNullOrEmpty(pageModel.NHS))
             {
-                page = 1;
-                mymodel.NHS = nhs;
+                roads = roads.Where(r => r.Nhs.Equals(pageModel.NHS));
             }
 
-            if (!String.IsNullOrEmpty(district))
-            {
-                roads = roads.Where(r => r.AhDistrict.Equals(district));
-            }
-            if (!String.IsNullOrEmpty(county)) 
-            {
-                roads = roads.Where(r => r.AhCounty.Equals(county));
-            }
-            if (!String.IsNullOrEmpty(route))
-            {
-                roads = roads.Where(r => r.AhRoute.Equals(route));
-            }
-            if (!String.IsNullOrEmpty(section))
-            {
-                roads = roads.Where(r => r.AhSection.Equals(section));
-            }
-            if (!logmile.Equals(null))
-            {
-                roads = roads.Where(r => r.AhBlm.Equals(logmile) || r.AhElm.Equals(logmile));
-            }
-            if (!blm.Equals(null))
-            {
-                roads = roads.Where(r => r.AhBlm.Equals(blm));//redundant
-            }
-            if (!elm.Equals(null))
-            {
-                roads = roads.Where(r => r.AhBlm.Equals(elm));
-            }
-            if (!String.IsNullOrEmpty(direction))
-            {
-                roads = roads.Where(r => r.LogDirect.Equals(direction));
-            }
-            if (!String.IsNullOrEmpty(nhs))
-            {
-                roads = roads.Where(r => r.Nhs.Equals(nhs));
-            }
-
-            switch (sortOrder)
+            switch (pageModel.SortOrder)
             {
                 case "name_desc":
                     roads = roads.OrderByDescending(r => r.Nhs);
                     break;
-                default: 
+                default:
                     roads = roads.OrderBy(r => r.Nhs);
                     break;
             }
-            mymodel.roadInvs = await roads.ToPagedListAsync(pageNumber, pageSize); 
-            mymodel.ExcludeNhs = await excNh.ToPagedListAsync(pageNumber, pageSize);
-            //mymodel.Dissolve = mymodel.DissolveSelect.;
-            if(mymodel.Dissolve=="Segment")
+            pageModel.roadInvs = await roads.ToPagedListAsync(pageNumber, pageSize);
+            pageModel.ExcludeNhs = await excNh.ToPagedListAsync(pageNumber, pageSize);
+
+            if (pageModel.Dissolve == "Segment")
             {
-                mymodel.DissolveNhsViews = await diss.ToPagedListAsync(pageNumber, pageSize);
+                pageModel.DissolveNhsViews = await diss.ToPagedListAsync(pageNumber, pageSize);
             }
 
-            return View(mymodel);
+            return View(pageModel);
         }
+        //public async Task<IActionResult> system_changes_nhs(string sortOrder, string district, string county, string route, string section, decimal? logmile, decimal? blm, decimal? elm, string direction, string nhs, int? page, string dissolve)
+        //{ //TODO: This function needs to be split up into smaller functions. It's taking way too many arguments. Could probably just pass an object as the parameter
+        //    int pageSize = 50;
+        //    int pageNumber = (page ?? 1); //TODO: separate paging for excludeNHS table
+
+        //    var roads = from r in _context.RoadInvs //select * from RoadInv where Nhs !=0
+        //                where r.Nhs != "0"
+        //                select r;
+
+        //    var excNh = from r in _context.ExcludeNhs 
+        //                select r;
+
+        //    var diss = from r in _context.DissolveNhsViews
+        //               select r;
+
+        //    var mymodel = new SystemChangesPageModel();
+        //    var validationModel = new ValidationModel(_context);
+
+        //    mymodel.Districts = validationModel.AH_District.ConvertAll(a => { return new SelectListItem { Text = a.DistrictNumber, Value = a.DistrictNumber, Selected = false }; }) ;
+        //    mymodel.Counties = validationModel.AH_County.ConvertAll(a => { return new SelectListItem { Text = a.CountyNumber + " - "+ a.County, Value = a.CountyNumber, Selected = false }; });
+        //    mymodel.Directions = validationModel.LOG_DIRECT.ConvertAll(a => { return new SelectListItem { Text = a.Domainvalue, Value = a.Domainvalue, Selected = false }; });
+        //    mymodel.NHS_vals = validationModel.NHS.ConvertAll(a => { return new SelectListItem { Text = a.Domainvalue, Value = a.Domainvalue, Selected = false }; });
+        //    mymodel.Dissolve = dissolve;
+
+
+
+        //    ViewBag.CurrentSort = sortOrder;
+
+        //    roads = roads.OrderBy(r => r.Nhs); 
+
+        //    if (district != null)
+        //    {
+        //        page = 1;
+        //        mymodel.District = district;
+        //    }
+        //    if (county != null)
+        //    {
+        //        page = 1;
+        //        mymodel.County = county;
+        //    }
+        //    if (route != null)
+        //    {
+        //        page = 1;
+        //        mymodel.Route = route;
+        //    }
+        //    if (section != null)
+        //    {
+        //        page = 1;
+        //        mymodel.Section = section;
+        //    }
+        //    if (logmile != null)
+        //    {
+        //        page = 1;
+        //        mymodel.Logmile = logmile;
+        //    }
+        //    if(blm != null)
+        //    {
+        //        page = 1;
+        //        mymodel.BLM = blm;
+        //    }
+        //    if (blm != null)
+        //    {
+        //        page = 1;
+        //        mymodel.ELM = elm;
+        //    }
+        //    if(direction != null)
+        //    {
+        //        page = 1;
+        //        mymodel.Direction = direction;
+        //    }
+        //    if(nhs != null)
+        //    {
+        //        page = 1;
+        //        mymodel.NHS = nhs;
+        //    }
+
+        //    if (!String.IsNullOrEmpty(district))
+        //    {
+        //        roads = roads.Where(r => r.AhDistrict.Equals(district));
+        //    }
+        //    if (!String.IsNullOrEmpty(county)) 
+        //    {
+        //        roads = roads.Where(r => r.AhCounty.Equals(county));
+        //    }
+        //    if (!String.IsNullOrEmpty(route))
+        //    {
+        //        roads = roads.Where(r => r.AhRoute.Equals(route));
+        //    }
+        //    if (!String.IsNullOrEmpty(section))
+        //    {
+        //        roads = roads.Where(r => r.AhSection.Equals(section));
+        //    }
+        //    if (!logmile.Equals(null))
+        //    {
+        //        roads = roads.Where(r => r.AhBlm.Equals(logmile) || r.AhElm.Equals(logmile));
+        //    }
+        //    if (!blm.Equals(null))
+        //    {
+        //        roads = roads.Where(r => r.AhBlm.Equals(blm));//redundant
+        //    }
+        //    if (!elm.Equals(null))
+        //    {
+        //        roads = roads.Where(r => r.AhBlm.Equals(elm));
+        //    }
+        //    if (!String.IsNullOrEmpty(direction))
+        //    {
+        //        roads = roads.Where(r => r.LogDirect.Equals(direction));
+        //    }
+        //    if (!String.IsNullOrEmpty(nhs))
+        //    {
+        //        roads = roads.Where(r => r.Nhs.Equals(nhs));
+        //    }
+
+        //    switch (sortOrder)
+        //    {
+        //        case "name_desc":
+        //            roads = roads.OrderByDescending(r => r.Nhs);
+        //            break;
+        //        default: 
+        //            roads = roads.OrderBy(r => r.Nhs);
+        //            break;
+        //    }
+        //    mymodel.roadInvs = await roads.ToPagedListAsync(pageNumber, pageSize); 
+        //    mymodel.ExcludeNhs = await excNh.ToPagedListAsync(pageNumber, pageSize);
+
+        //    if(mymodel.Dissolve=="Segment")
+        //    {
+        //        mymodel.DissolveNhsViews = await diss.ToPagedListAsync(pageNumber, pageSize);
+        //    }
+
+        //    return View(mymodel);
+        //}
 
         //[HttpGet] // this action result returns the partial containing the modal
         //public ActionResult EditDesignation(int id)
